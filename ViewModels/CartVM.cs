2490a11e5cfde6546895b99cede5170f2e3ad470
Robert Levelton <rlevelton@gmail.com>
2025-01-14 13:39:46 -0800

    added delete from cart function

--- ViewModels/CartVM.cs
+++ ViewModels/CartVM.cs
@@ -1,6 +1,20 @@
-﻿namespace GreenHiTech.ViewModels
+﻿using GreenHiTech.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreenHiTech.ViewModels
 {
     public class CartVM
     {
+        [Display(Name = "Cart ID")]
+        public int PkId { get; set; }
+
+        [Display(Name = "User ID")]
+        public int FkUserId { get; set; }
+
+        [Display(Name = "Total Amount")]
+        public decimal TotalAmount { get; set; }
+
+        [Display(Name = "Products")]
+        public virtual ICollection<CartProduct> CartProducts { get; set; } = new List<CartProduct>();
     }
 }
