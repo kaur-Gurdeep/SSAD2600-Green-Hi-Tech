b663a8a5fce9b268229d118c969951bfd6bde192
Sangeeta Panwar <panwarsangeeta04@gmail.com>
2025-01-14 13:40:00 -0800

    orderVM and orderController  added

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenHiTech.Controllers
+{
+    public class OrderController: Controller
+    {
+        private readonly OrderRepo _orderRepo;
+        private readonly OrderController _logger;
+
+        public OrderController(ILogger<OrderController> logger, OrderRepo orderRepo)
+        {
+            _logger = logger;
+            _orderRepo = orderRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(payPalConfirmationModel payPalConfirmation) 
+        {
+
+        }
+    }
+}
---
+++ ViewModels/OrderVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using GreenHiTech.Models;
+
+namespace GreenHiTech.ViewModels;
+
+public partial class OrderVM
+{
+    public int PkId { get; set; }
+
+    public int FkUserId { get; set; }
+
+    public DateOnly OrderDate { get; set; }
+
+    public decimal TotalAmount { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public virtual User FkUser { get; set; } = null!;
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
+    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+}
+
