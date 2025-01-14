483df92c1d764b7bafce5abc4bd748bc5edde245
Robert Levelton <rlevelton@gmail.com>
2025-01-14 11:37:24 -0800

    connected CartProduct files

--- Controllers/CartProductController.cs
+++ Controllers/CartProductController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GreenHiTech.Repositories;
+using GreenHiTech.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GreenHiTech.Controllers
 {
     public class CartProductController : Controller
     {
+        private readonly CartProductRepo _cartProductRepo;
+
+        public CartProductController(CartProductRepo cartProductRepo)
+        {
+            _cartProductRepo = cartProductRepo;
+        }
         public IActionResult Index()
         {
+            IEnumerable<CartProductVM> cartProducts = _cartProductRepo.GetAll();
             return View();
         }
     }
--- Repositories/CartProductRepo.cs
+++ Repositories/CartProductRepo.cs
@@ -1,6 +1,27 @@
-﻿namespace GreenHiTech.Repositories
+﻿using GreenHiTech.Data;
+using GreenHiTech.ViewModels;
+
+namespace GreenHiTech.Repositories
 {
     public class CartProductRepo
     {
+        private readonly ApplicationDbContext _context;
+
+        public CartProductRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<CartProductVM> GetAll()
+        {
+            IEnumerable<CartProductVM> cartProducts = _context.cartProducts.Select(cp => new CartProductVM
+            {
+                PkId = cp.PkId,
+                FkCartId = cp.FkCartId,
+                FkProductId = cp.FkProductId,
+                Quantity = cp.Quantity
+            });
+            return cartProducts;
+        }
     }
 }
