18332e5992247a96024295373dbde89e3b682451
Robert Levelton <rlevelton@gmail.com>
2025-01-21 11:51:08 -0800

    fixed weird file errors

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,26 +1,26 @@
-﻿//using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-//namespace GreenHiTech.Controllers
-//{
-//    public class OrderController: Controller
-//    {
-//        private readonly OrderRepo _orderRepo;
-//        private readonly OrderController _logger;
+namespace GreenHiTech.Controllers
+{
+    public class OrderController: Controller
+    {
+        private readonly OrderRepo _orderRepo;
+        private readonly OrderController _logger;
 
-//        public OrderController(ILogger<OrderController> logger, OrderRepo orderRepo)
-//        {
-//            _logger = logger;
-//            _orderRepo = orderRepo;
-//        }
+        public OrderController(ILogger<OrderController> logger, OrderRepo orderRepo)
+        {
+            _logger = logger;
+            _orderRepo = orderRepo;
+        }
 
-//        public IActionResult Index()
-//        {
-//            return View();
-//        }
-//        [HttpPost]
-//        public IActionResult Create(payPalConfirmationModel payPalConfirmation) 
-//        {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(payPalConfirmationModel payPalConfirmation) 
+        {
 
-//        }
-//    }
-//}
+        }
+    }
+}
