1c6167b72476a8cb710f47abb23c2d1d656b3aa1
Jae Young Ahn <jahn35@my.bcit.ca>
2025-01-21 20:30:01 -0800

    oder detail

--- Controllers/OrderDetailController.cs
+++ Controllers/OrderDetailController.cs
@@ -50,5 +50,57 @@ namespace GreenHiTech.Controllers
             return CreatedAtAction(nameof(GetOrderDetail), new { id = orderDetail.PkId }, orderDetail);
         }
 
+        // PUT: api/OrderDetail/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderDetail(int id, OrderDetail orderDetail)
+        {
+            if (id != orderDetail.PkId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orderDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/OrderDetail/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderDetail(int id)
+        {
+            var orderDetail = await _context.OrderDetails.FindAsync(id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderDetails.Remove(orderDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderDetailExists(int id)
+        {
+            return _context.OrderDetails.Any(e => e.PkId == id);
+        }
+
+
     }
 }
