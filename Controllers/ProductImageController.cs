9f13a0dcaf8d106502b4aaad05cabc32d83b36a8
Marcosh1515 <m.baron.campeau@gmail.com>
2025-01-24 09:58:41 -0800

    adding images for ProductImages

---
+++ Controllers/ProductImageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenHiTech.Controllers
+{
+    public class ProductImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
